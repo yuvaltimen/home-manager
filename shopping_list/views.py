cb0cfa50c6c368360b119445e71d05158a7ac843
Yuval Timen <yuvaltimen@gmail.com>
2024-08-15 11:57:42 -0400

    Fix timezone and daylight savings issue

--- shopping_list/views.py
+++ shopping_list/views.py
@@ -1,29 +1,14 @@
 import datetime
+
+import pytz
 from pyluach.dates import HebrewDate
 from pyluach import parshios
 
 from django.shortcuts import render
 from .models import GroceryItem
 
-dt = datetime.datetime.now()
-hebrew_dt = HebrewDate.from_pydate(dt)
-parsha = parshios.getparsha_string(hebrew_dt)
-ctx = {
-    'dt': dt,
-    'hebrew_dt': hebrew_dt,
-    'parsha': parsha,
-    'ivrit_day': hebrew_dt.hebrew_date_string()
-}
 
-def home(req):
-    ctx.update({
-        'title': 'Shopping List',
-        'shopping_items': GroceryItem.objects.order_by('is_checked', 'name')
-    })
-    return render(req, 'shopping_list/home.html', ctx)
-
-
-def about(req):
+def get_date_time_ctx():
     dt = datetime.datetime.now()
     hebrew_dt = HebrewDate.from_pydate(dt)
     parsha = parshios.getparsha_string(hebrew_dt)
@@ -33,6 +18,20 @@ def about(req):
         'parsha': parsha,
         'ivrit_day': hebrew_dt.hebrew_date_string()
     }
+    return ctx
+
+
+def home(req):
+    ctx = get_date_time_ctx()
+    ctx.update({
+        'title': 'Shopping List',
+        'shopping_items': GroceryItem.objects.order_by('is_checked', 'name')
+    })
+    return render(req, 'shopping_list/home.html', ctx)
+
+
+def about(req):
+    ctx = get_date_time_ctx()
     return render(req, 'shopping_list/about.html', ctx)
 
 
