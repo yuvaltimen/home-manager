2b76219b526880497fd24c7d0416efd0ff9df051
Yuval Timen <yuvaltimen@gmail.com>
2024-08-14 20:43:02 -0400

    Add user registration form

--- home_manager/users/views.py
+++ home_manager/users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
-# Create your views here.
+def register(req):
+    if req.method == "POST":
+        form = UserRegisterForm(req.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(req, f"Account created for {username}")
+            return redirect('shopping_list_home')
+    else:
+        form = UserRegisterForm()
+    return render(req, 'users/register.html', {'form': form})
+
