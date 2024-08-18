e19d7199979bd1d67ece40da3f72aa740521575d
Yuval Timen <yuvaltimen@gmail.com>
2024-08-18 14:04:32 -0400

    Add moon phase mappings and images

--- app/templatetags/extra_tags.py
+++ app/templatetags/extra_tags.py
@@ -7,3 +7,9 @@ register = template.Library()
 @register.filter(name='zip')
 def zip_lists(a, b):
     return zip_longest(a, b)
+
+
+@register.filter(name='replace_with_space')
+def replace_with(value, to_replace):
+    return value.replace(to_replace, " ")
+
--- app/views.py
+++ app/views.py
@@ -1,4 +1,7 @@
 import datetime
+from pytz import timezone
+from skyfield.api import load
+from skyfield.framelib import ecliptic_frame
 from pyluach.dates import HebrewDate
 from pyluach import parshios
 
@@ -16,17 +19,57 @@ from django.contrib.auth.models import User
 from .models import GroceryItem, Reminder
 
 
+def get_moon_phase(current_dt):
+
+    ts = load.timescale()
+    t = ts.from_datetime(timezone('US/Eastern').localize(current_dt))
+
+    eph = load('de421.bsp')
+    sun, moon, earth = eph['sun'], eph['moon'], eph['earth']
+
+    e = earth.at(t)
+    s = e.observe(sun).apparent()
+    m = e.observe(moon).apparent()
+
+    _, slon, _ = s.frame_latlon(ecliptic_frame)
+    _, mlon, _ = m.frame_latlon(ecliptic_frame)
+    phase = (mlon.degrees - slon.degrees) % 360.0
+
+    if phase < 0.2:
+        return "New_Moon"
+    elif 0.2 < phase < 89.8:
+        return "Waxing_Crescent"
+    elif 89.8 <= phase < 90.2:
+        return "First_Quarter"
+    elif 90.2 <= phase < 179.8:
+        return "Waxing_Gibbous"
+    elif 179.8 <= phase < 180.2:
+        return "Full_Moon"
+    elif 180.2 <= phase < 269.8:
+        return "Waning_Gibbous"
+    elif 269.8 <= phase <= 270.2:
+        return "Last_Quarter"
+    elif phase < 360.0:
+        return "Waning_Crescent"
+    else:
+        return "Unknown"
+
+
 def get_date_time_ctx():
     dt = datetime.datetime.now()
+    moon_phase = get_moon_phase(dt)
     hebrew_dt = HebrewDate.from_pydate(dt)
     parsha = parshios.getparsha_string(hebrew_dt)
     ctx = {
         'dt': dt,
+        'moon_phase': moon_phase,
         'hebrew_dt': hebrew_dt,
         'parsha': parsha,
         'ivrit_day': hebrew_dt.hebrew_date_string()
     }
     return ctx
+
+
 
 
 ########################
